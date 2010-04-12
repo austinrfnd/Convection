9e45285ccb2dd4077d579401c7cab86c7d8e4e34
Austin Fonacier <austinrf@gmail.com>
2010-04-11 17:11:00 -0700

    Fixing view specs

--- spec/views/documents/new.html.haml_spec.rb
+++ spec/views/documents/new.html.haml_spec.rb
@@ -11,7 +11,7 @@ describe "/documents/new.html.haml" do
   it "should render new form" do
     render "/documents/new.html.haml"
     
-    response.should have_tag("form[action=?][method=post]", documents_path) do
+    response.should have_tag("form[action=?][method=post]", documents_path("X-Progress-ID" => '12345')) do
       with_tag("input#document_name[name=?]", "document[name]")
       with_tag("textarea#document_description[name=?]", "document[description]")
     end
